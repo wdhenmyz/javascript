902890b73744f87363fdc63cfff0551d6670ea99
wdhenmyz <wesllen.dhenmyz@outlook.com>
2024-01-29 11:29:12 -0300

    commit

--- sorteio 2.js
+++ sorteio 2.js
@@ -1,13 +1,22 @@
-INICIAR 
-    FAZER palavraMisteriosa = [j,a,v,a,s,c,r,i,p,t], palavraUsuario[]
-    FAZER inputUsuario=0 ;
-    FAZER contador
-    ENQUANTO (palavraUsuario!=palavraMisteriosa) FAÇA{
-        LER inputUsuario
-        FAZER palavraUsuario.push(inputUsuario);
-            SE palavraUsuario[inputUsuario] == palavraMisteriosa[contador];
-            ESCREVER palavraMisteriosa [contador];
-                SENAO contador++;
+//    var palavraMisteriosa = ['j','a','v','a','s','c','r','i','p','t'] 
+ //   var palavraUsuario = []
+ //   var inputUsuario=0 ;
+ //   var contador
+ //   while (palavraUsuario!=palavraMisteriosa){
+ //       inputUsuario
+ //       palavraUsuario.push(inputUsuario);
+ //           if(palavraUsuario[inputUsuario] == palavraMisteriosa[contador]){
+  //          console.log(palavraMisteriosa [contador]);
+ //           }else contador++;
 
+var palavraMisteriosa = [j,a,v,a,s,c,r,i,p,t], palavraDescoberta[];
+var tamanho = palavraMisteriosa.lenght;
+var inputUsuario;
+var contador = 0;
+while (palavraDescoberta!=palavraMisteriosa){
+    inputUsuario
+    for(contador=0, contador<tamanho)
+        if (palavraDescoberta[contador] == inputUsuario){;
+            palavraDescoberta[contador] = inputUsuario;
+        }
 }
-FINALIZR
