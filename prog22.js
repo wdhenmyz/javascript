d07c6dc6bc0345291f168c8a73e21d194cff0a44
wdhenmyz <wesllen.dhenmyz@outlook.com>
2024-02-01 11:55:43 -0300

    yes

--- prog22.js
+++ prog22.js
@@ -19,18 +19,8 @@ for (var i=0;i<notas.length;i++){
     console.log(acum)
   } 
 }
+soma()
 
 //calcule a média 3 = 2/1
-function media(){
-for (var i=0;i<notas.length;i++){ //varre todos os índices da matriz (vetor externo)
-    acum=0; //limpa a nota 
-    for (var j=1;j<notas[0].length;j++){ //varre os índices do vetor que está na posição i (vetor interno) 
-        acum=acum+Number(notas[i][j]);
-    }
-    //calcula e escreve o resultado da média
-    console.log(notas[i][0]+" "+ (acum/(notas[0].length-1)));
-  }
-}
-
-soma()
+function media()
 media()
